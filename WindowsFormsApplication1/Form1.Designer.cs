1d24a9717ec9d9b12d6277c61cf0f686055918a6
Ran Lu <ranluoverflow@hotmail.com>
2017-03-16 14:13:44 +0800

    Features: (1) Add virtual paths to nginx conf; (2) Remove virtual paths from nginx conf.
    Target Platform: .NET 2.0

--- WindowsFormsApplication1/Form1.Designer.cs
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -29,26 +29,51 @@
         private void InitializeComponent()
         {
             this.btnAddPath = new System.Windows.Forms.Button();
+            this.lbPathList = new System.Windows.Forms.ListBox();
+            this.btnDeletePaths = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btnAddPath
             // 
-            this.btnAddPath.Location = new System.Drawing.Point(39, 218);
+            this.btnAddPath.Location = new System.Drawing.Point(12, 218);
             this.btnAddPath.Name = "btnAddPath";
-            this.btnAddPath.Size = new System.Drawing.Size(75, 23);
+            this.btnAddPath.Size = new System.Drawing.Size(124, 23);
             this.btnAddPath.TabIndex = 0;
             this.btnAddPath.Text = "添加虚拟路径";
             this.btnAddPath.UseVisualStyleBackColor = true;
             this.btnAddPath.Click += new System.EventHandler(this.btnAddPath_Click);
+            // 
+            // lbPathList
+            // 
+            this.lbPathList.FormattingEnabled = true;
+            this.lbPathList.ItemHeight = 12;
+            this.lbPathList.Location = new System.Drawing.Point(12, 18);
+            this.lbPathList.Name = "lbPathList";
+            this.lbPathList.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
+            this.lbPathList.Size = new System.Drawing.Size(260, 184);
+            this.lbPathList.TabIndex = 1;
+            // 
+            // btnDeletePaths
+            // 
+            this.btnDeletePaths.Location = new System.Drawing.Point(142, 218);
+            this.btnDeletePaths.Name = "btnDeletePaths";
+            this.btnDeletePaths.Size = new System.Drawing.Size(130, 23);
+            this.btnDeletePaths.TabIndex = 2;
+            this.btnDeletePaths.Text = "删除虚拟路径";
+            this.btnDeletePaths.UseVisualStyleBackColor = true;
+            this.btnDeletePaths.Click += new System.EventHandler(this.btnDeletePaths_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.btnDeletePaths);
+            this.Controls.Add(this.lbPathList);
             this.Controls.Add(this.btnAddPath);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "文件服务器虚拟路径配置工具";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
         }
@@ -56,6 +81,8 @@
         #endregion
 
         private System.Windows.Forms.Button btnAddPath;
+        private System.Windows.Forms.ListBox lbPathList;
+        private System.Windows.Forms.Button btnDeletePaths;
     }
 }
 
--- WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/Form1.cs
@@ -3,25 +3,188 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
-using System.Linq;
+//using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+//using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+
 
 namespace WindowsFormsApplication1
 {
+
     public partial class Form1 : Form
     {
+        private string path = ".\\nginx.conf";
+        private string as_start_marker = "#as_start";
+        private string as_end_marker = "#as_end";
+        private string as_meat_marker = "#as_meat";
+        private char[] delimiters = new char[] { ' ', '\t' };
+
+        private string buf_path = ".\\nginx_tmp.conf";
+
         public Form1()
         {
             InitializeComponent();
         }
 
+        public string[] FilterOutEmptyString(string[] input)
+        {
+            List<string> availableCars = new List<string>();
+
+            foreach (string c in input)
+            {
+                if (c.Length > 0)
+                    availableCars.Add(c);
+            }
+
+            return availableCars.ToArray();
+        }
+
+        private void loadPaths()
+        {
+            lbPathList.Items.Clear();
+
+            StreamReader sr = new StreamReader(path, Encoding.Default);
+            String line;
+            while ((line = sr.ReadLine()) != null)
+            {
+                //Console.WriteLine(line.ToString());
+
+                int index = line.IndexOf(as_meat_marker);
+                if (-1 != index)
+                {
+                    line = line.Substring(0, index);
+                    line = line.Trim();
+                    Console.WriteLine("line:" + line);
+                    //string[] pieces = line.Split(delimiters);
+
+                    line = line.Replace(';', ' ');
+                    string[] pieces = System.Text.RegularExpressions.Regex.Split(line, "\\s+");
+                    
+                    //pieces.Where(p => p.Length > 0);
+                    pieces = FilterOutEmptyString(pieces);
+
+                    foreach (string i in pieces)
+                    {
+                        Console.WriteLine("piece:" + i);
+                    }
+                    lbPathList.Items.Add(pieces[1]);
+                }
+            }
+            sr.Close();
+        }
+
+        private void savePaths()
+        {
+            FileStream fs = new FileStream(buf_path, FileMode.Create);
+            StreamWriter sw = new StreamWriter(fs, Encoding.Default);
+
+            StreamReader sr = new StreamReader(path, Encoding.Default);
+            String line;
+            while ((line = sr.ReadLine()) != null)
+            {
+                //Console.WriteLine(line.ToString());
+
+                int index = line.IndexOf(as_start_marker);
+                if (-1 != index)
+                {
+                    sw.WriteLine(line);
+
+                    ///
+                    foreach (string i in lbPathList.Items)
+                    {
+                        sw.WriteLine("location / {");
+
+
+                        string new_path = i.Replace('\\', '/');
+
+                        sw.WriteLine("\troot\t" + new_path + ";" + as_meat_marker);
+                        sw.WriteLine("}");
+                    }
+
+                    ///
+                    while (true)
+                    {
+                        line = sr.ReadLine();
+                        if (-1 != line.IndexOf(as_end_marker))
+                            break;
+                    }
+                    ///
+                    sw.WriteLine(line);
+                }
+                else
+                {
+                    sw.WriteLine(line);
+                }
+
+            }
+            sr.Close();
+            
+            //清空缓冲区
+            sw.Flush();
+            //关闭流
+            sw.Close();
+            fs.Close();
+
+            //
+            File.Delete(path);
+            File.Move(buf_path, path);
+        }
+
         private void btnAddPath_Click(object sender, EventArgs e)
         {
 
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            //fbd.SelectedPath = "D:\\tmp";
+            if(fbd .ShowDialog() == DialogResult.OK)
+            {
+                string selected_path = fbd.SelectedPath;
 
+                lbPathList.Items.Add(selected_path);
 
+                savePaths();
+                loadPaths();
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            loadPaths();
+        }
+
+        private void deletePaths()
+        {
+            ListBox.SelectedIndexCollection sic = lbPathList.SelectedIndices;//得到选择的Item的下标
+
+            if (sic.Count == 0)
+                return;
+            else
+            {
+                //  将选择的Item放入list中
+                List<int> list = new List<int>();
+                for (int i = 0; i < sic.Count; i++)
+                {
+                    list.Add(sic[i]);
+                }
+                list.Sort();//对list进行排序（库里默认的排序结果一般指的是从下到大的排序）
+
+                while (list.Count != 0)//按照下标从大到小的顺序从ListBox控件里删除选择的Item
+                //如果这里采用其它顺序则可能破坏下标的有效性
+                {
+                    lbPathList.Items.RemoveAt(list[list.Count - 1]);
+                    list.RemoveAt(list.Count - 1);
+                }
+            }
+
+            savePaths();
+            loadPaths();
+        }
+
+        private void btnDeletePaths_Click(object sender, EventArgs e)
+        {
+            deletePaths();
         }
     }
 }
